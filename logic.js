aff8e3f5706a56cb2fa1bdaedb8ced34524f4ec5
K. Ngo <220795382+kpngo1012@users.noreply.github.com>
2026-07-14 21:41:39 -0700

    Add functionality to run game in a loop with user selecting cells, no check win condition yet

--- logic.js
+++ logic.js
@@ -1,6 +1,7 @@
 //Gameboard represents the state of the gameboard
 //Each square holds a Cell
 //expose a method for dropping a token
+const prompt = require("prompt-sync")({sigint: true}); 
 
 function Gameboard() {
   const board = [];
@@ -15,13 +16,17 @@ function Gameboard() {
     }
   }
 
-  //now that we have the board
-  //how do we find all the available cells?
-  //available cells: all the cells that are empty aka cell.value = 0;
+  // const getValidCells = () => {
+  //   const validCells = board.filter((row) => row.filter((cell) => cell.getValue() === 0));
+  //   return validCells;}
+
   const getValidCells = () => {
-    const validCells = board.filter((row) => row.filter((cell) => cell.getValue() === 0));
-    console.log(validCells);
-    return validCells;
+    const arr = [];
+
+    board.map((row) => row.map((cell) => { 
+      if (cell.getValue() === 0) arr.push(cell)
+    } ));
+    return arr;
   }
 
 
@@ -39,8 +44,8 @@ function Cell() {
   let value = 0;
 
   //accept a player's token to change the token of the cell
-  const addToken = (player, row, col) => {
-    value = player;
+  const addToken = (player) => {
+    value = player.token;
   }
 
   //method to get the current method
@@ -52,12 +57,7 @@ function Cell() {
   }
 }
 
-function GameController() {
-  //turn 0 = first player
-  //turn 1 = second player
-  //it's counterintuiive due to how array works
-  const turn = 1;
-
+function Players() {
   const players = [
     {
       name: "player 1",
@@ -69,50 +69,98 @@ function GameController() {
     }
   ];
 
+  //get current active player
   const getActivePlayer = (turn) => players[turn-1];
+
+  //switch turns - take the current turn and output the next one
   const switchActivePlayer = (player) => player.token === 1 ? players[1] : players[0];
+
+  return { getActivePlayer, switchActivePlayer }
+}
+
+function playRound(turn, board) {
+
+  const currPlayers = Players();
+  const activePlayer = currPlayers.getActivePlayer(turn);
+
+  const isValid = (input) => {
+    if 
+    (input && 
+     input.length === 1 && 
+     (parseInt(input) >= 0 && parseInt(input) <= 2)) 
+     
+     { return true }
+    else {
+      console.log(`Invalid input. Try again.`)
+      return false;
+  }}
+
+  let getRow, getCol;
+
+  const searchCell = (row, col) => {
+    const gameboard = board.getBoard();
+
+    for (let i = 0; i < 3; i++) {
+      if (i === row) {
+        for (let j = 0; j < 3; j++) {
+          if (j === col) {
+            console.log(`Player position: ${row}, ${col}`);
+            console.log(`Gameboard cell: ${i}, ${j}`);
+            return gameboard[i][j];
+    }}}}
+  }
+
+  //change this variable name later to not be confused with checking valid userInput
+  const checkEmpty = (cell) => cell.getValue() === 0 ? true : false;
+  const makeMove = (cell, player) => {
+    console.log(`Before add token: ${cell.getValue()}`)
+    console.log(`Current player: ${player.name}`)
+    if (checkEmpty(cell)) cell.addToken(player);
+    console.log(`After drop token: ${cell.getValue()}`);
+  }
+
+  do {
+    getRow = prompt("Choose a row (0-2): ", 0 );
+  } while (!(isValid(getRow)));
+
+  do {
+    getCol = prompt("Choose a col (0-2): ", 0 );
+  } while (!(isValid(getCol)));
+
+  // console.log(getRow);
+  // console.log(getCol);
+  // console.log(activePlayer);
+  // const foundCell = searchCell(parseInt(getRow), parseInt(getCol));
+  // makeMove(foundCell, activePlayer);
+}
+
+function GameController() {
+  const switchTurn = turn => (turn === 1) ? 2 : 1;
 
   //initialize the board
   const board = Gameboard();
 
-  //print out the board so we can see
+  //print out the board so we can see 
   const printBoard = () => {
     const boardWithCellValues = board.getBoard().map((row) => row.map((cell) => cell.getValue()))
     console.log(boardWithCellValues);
     };
 
-  const playRound = () => {
-    //get current active player turn = 1
-    const activePlayer = getActivePlayer(1);
-    //get all the valid cells so the player can make a move
-    const availCells = board.getValidCells();
-    //get player position
-    const position = [0, 0];
-    const isValid = (position, availCells) => availCells.includes()
 
-    //you can do this
-    //you can figure this out and you can complete this project
-    //keep pushing
-    console.log(activePlayer);
-    console.log(availCells);
+  let availCells = board.getValidCells();
+  let turn = 1;
 
-  }
-
-  return { printBoard, getActivePlayer, switchActivePlayer, playRound }
+  // while (availCells.length > 0) {
+  //   console.log(`Turn ${turn}`)
+  //   playRound(turn, board);
+  //   printBoard();
+  //   turn = switchTurn(turn);
+  //   availCells = board.getValidCells();
+  // }
+  playRound(turn, board)
+  return { printBoard, playRound }
 }
 
 let game = GameController();
-game.playRound();
-//now that 
-
-// const testBoard = Gameboard();
-// console.log(testBoard.printValidCells())
-//for game controller
-//initialize meaning draw the board
-//find empty cells
-//let player 1 pick the cell
-//drop the token aka change value from 0 to 1
-//check win condition in between
-//switch turns
 
 
